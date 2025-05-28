c430afbdd7bad1697d97c8f0ccc829303eb6fdf5
phanirithvij <phanirithvij2000@gmail.com>
2025-05-28 11:20:58 +0530

    tampermonkey script
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- shortcut.js
+++ shortcut.js
@@ -1,46 +1,58 @@
-const repo = "Defelo/nixpkgs-review-gha";
+// ==UserScript==
+// @name         nixpkgs-review-gha shortcut.js
+// @namespace    http://tampermonkey.net/
+// @version      2025-05-28
+// @description  try to take over the world!
+// @author       You
+// @match        https://github.com/NixOS/nixpkgs/pull/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
 
-const sleep = duration => new Promise(resolve => setTimeout(resolve, duration));
-const query = async (doc, sel) => {
-  while (true) {
-    const elem = doc.querySelector(sel);
-    if (elem !== null) return elem;
-    await sleep(100);
-  }
-};
+(function() {
+    'use strict';
+    const repo = "phanirithvij/nixpkgs-review-gha";
 
-const setup = async () => {
-  const match = /^https:\/\/github.com\/nixos\/nixpkgs\/pull\/(\d+)/i.exec(location.href);
-  if (match === null) return;
-  
-  const pr = match[1];
-  const actions = await query(document, ".gh-header-show .gh-header-actions");
-
-  if (actions.querySelector(".run-nixpkgs-review") === null) {
-    const btn = document.createElement("button");
-    btn.classList.add("Button", "Button--secondary", "Button--small", "run-nixpkgs-review");
-    btn.innerText = "Run nixpkgs-review";
-    actions.prepend(btn);
-    btn.onclick = () => {
-      const w = window.open(`https://github.com/${repo}/actions/workflows/review.yml`);
-      w.addEventListener("load", async () => {
-        (await query(w.document, "details > summary.btn")).click();
-        (await query(w.document, "input.form-control[name='inputs[pr]']")).value = pr;
-      });
+    const sleep = duration => new Promise(resolve => setTimeout(resolve, duration));
+    const query = async (doc, sel) => {
+        while (true) {
+            const elem = doc.querySelector(sel);
+            if (elem !== null) return elem;
+            await sleep(100);
+        }
     };
-  }
 
-  if (actions.querySelector(".goto-pr-tracker") === null) {
-    const btn = document.createElement("button");
-    btn.classList.add("Button", "Button--secondary", "Button--small", "goto-pr-tracker");
-    btn.innerText = "PR Tracker";
-    actions.prepend(btn);
-    btn.onclick = () => {
-      // window.open(`https://nixpk.gs/pr-tracker.html?pr=${pr}`);
-      window.open(`https://nixpkgs-tracker.ocfox.me/?pr=${pr}`);
-    };
-  }
-}
+    const setup = async () => {
+        const match = /^https:\/\/github.com\/nixos\/nixpkgs\/pull\/(\d+)/i.exec(location.href);
+        if (match === null) return;
 
-navigation.addEventListener('navigate', setup);
-setup();
+        const pr = match[1];
+        const actions = await query(document, ".gh-header-show .gh-header-actions");
+
+        if (actions.querySelector(".run-nixpkgs-review") === null) {
+            const btn = document.createElement("button");
+            btn.classList.add("Button", "Button--secondary", "Button--small", "run-nixpkgs-review");
+            btn.innerText = "Run nixpkgs-review";
+            actions.prepend(btn);
+            btn.onclick = () => {
+                const w = window.open(`https://github.com/${repo}/actions/workflows/review.yml`);
+                w.addEventListener("load", async () => {
+                    (await query(w.document, "details > summary.btn")).click();
+                    (await query(w.document, "input.form-control[name='inputs[pr]']")).value = pr;
+                });
+            };
+        }
+
+        if (actions.querySelector(".goto-pr-tracker") === null) {
+            const btn = document.createElement("button");
+            btn.classList.add("Button", "Button--secondary", "Button--small", "goto-pr-tracker");
+            btn.innerText = "PR Tracker";
+            actions.prepend(btn);
+            btn.onclick = () => {
+                // window.open(`https://nixpk.gs/pr-tracker.html?pr=${pr}`);
+                window.open(`https://nixpkgs-tracker.ocfox.me/?pr=${pr}`);
+            };
+        }
+    }
+    setup();
+})();
